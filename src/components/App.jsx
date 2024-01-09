1d5530e878439ccb8ffe558bf01abb80643d735a
Just-me-TTA <129186821+Just-me-TTA@users.noreply.github.com>
2024-01-09 16:37:02 +0200

    хуканули

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { GlobalStyle } from 'GlobalStyle';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { serviceSearch } from 'services/pixabay-api';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -8,110 +8,93 @@ import { Modal } from './Modal/Modal';
 import { Error, Wrapper } from './App.styled';
 import { Loader } from './Loader/Loader';
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    currentItem: null,
-    isLoading: false,
-    isModalOpen: false,
-    isLoadMore: false,
-    isSearchDisabled: false,
-    error: '',
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [currentItem, setCurrentItem] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoadMore, setIsLoadMore] = useState(false);
+  const [isSearchDisabled, setIsSearchDisabled] = useState(false);
+  const [error, setError] = useState('');
 
-  componentDidUpdate(_, prevState) {
-    const { page, query } = this.state;
-    if (page !== prevState.page || query !== prevState.query) {
-      this.setState({ isLoading: true, isSearchDisabled: true });
-      serviceSearch(query, page)
-        .then(({ hits, totalHits }) => {
-          if (!hits.length) {
-            this.setState({
-              error:
-                'Sorry, there are no images matching your search query. Please try again.',
-            });
-            return;
-          }
-          this.setState(prev => ({
-            images: [...prev.images, ...hits],
-            isLoadMore: this.state.page < Math.ceil(totalHits / 12),
-            error: '',
-          }));
-        })
-        .catch(error =>
-          this.setState({
-            error: 'Sorry, something went wrong. Please try again later.',
-          })
-        )
-        .finally(() =>
-          this.setState({ isLoading: false, isSearchDisabled: false })
-        );
-    }
-  }
-
-  handleSearch = obj => {
-    if (obj.searchQuery.trim() === '') {
-      this.setState({
-        error: 'Please, enter your query',
-      });
+  useEffect(() => {
+    if (!query) {
       return;
     }
-    this.setState({
-      query: obj.searchQuery,
-      page: 1,
-      images: [],
-      isLoadMore: false,
-      error: '',
-    });
+    setIsLoading(true);
+    setIsSearchDisabled(true);
+
+    serviceSearch(query, page)
+      .then(({ hits, totalHits }) => {
+        if (!hits.length) {
+          setError(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+          return;
+        }
+        setImages(prev => [...prev, ...hits]);
+        setIsLoadMore(page < Math.ceil(totalHits / 12));
+        setError('');
+      })
+      .catch(error =>
+        setError('Sorry, something went wrong. Please try again later.')
+      )
+      .finally(() => {
+        setIsLoading(false);
+        setIsSearchDisabled(false);
+      });
+  }, [page, query]);
+
+  const handleSearch = obj => {
+    if (obj.searchQuery.trim() === '') {
+      setError('Please, enter your query');
+      return;
+    }
+
+    if (obj.searchQuery.trim() === query) {
+      return;
+    }
+
+    setQuery(obj.searchQuery.trim());
+    setPage(1);
+    setImages([]);
+    setIsLoadMore(false);
+    setError('');
   };
 
-  handleLoadMore = () => {
-    this.setState(prev => ({ page: prev.page + 1 }));
+  const handleLoadMore = () => {
+    setPage(prev => prev + 1);
   };
 
-  handleOpenModal = e => {
+  const handleOpenModal = e => {
     const currentImageId = Number(e.target.id);
-    const currentItem = this.state.images.find(
-      ({ id }) => id === currentImageId
-    );
-    this.setState({ currentItem: currentItem, isModalOpen: true });
+    const currentItem = images.find(({ id }) => id === currentImageId);
+    setCurrentItem(currentItem);
+    setIsModalOpen(true);
   };
 
-  closeModal = () => {
-    this.setState({ currentItem: null, isModalOpen: false });
+  const closeModal = () => {
+    setCurrentItem(null);
+    setIsModalOpen(false);
   };
 
-  render() {
-    const {
-      images,
-      isModalOpen,
-      isLoadMore,
-      isLoading,
-      isSearchDisabled,
-      currentItem,
-      error,
-    } = this.state;
-
-    return (
-      <Wrapper>
-        <GlobalStyle />
-        <Searchbar
-          handleSearch={this.handleSearch}
-          isSearchDisabled={isSearchDisabled}
-        />
-        {error === '' ? (
-          <ImageGallery items={images} handleOpenModal={this.handleOpenModal} />
-        ) : (
-          <Error>{error}</Error>
-        )}
-        {isLoading && <Loader />}
-        {isLoadMore && <Button onClick={this.handleLoadMore} />}
-        {isModalOpen && (
-          <Modal item={currentItem} closeModal={this.closeModal} />
-        )}
-      </Wrapper>
-    );
-  }
-}
+  return (
+    <Wrapper>
+      <GlobalStyle />
+      <Searchbar
+        handleSearch={handleSearch}
+        isSearchDisabled={isSearchDisabled}
+      />
+      {error === '' ? (
+        <ImageGallery items={images} handleOpenModal={handleOpenModal} />
+      ) : (
+        <Error>{error}</Error>
+      )}
+      {isLoading && <Loader />}
+      {isLoadMore && <Button onClick={handleLoadMore} />}
+      {isModalOpen && <Modal item={currentItem} closeModal={closeModal} />}
+    </Wrapper>
+  );
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,39 +1,35 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { Backdrop, ModalContainer } from './Modal.styled';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  handleBackdropClose = e => {
+export const Modal = ({ item, closeModal }) => {
+  const handleBackdropClose = e => {
     if (e.target === e.currentTarget) {
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  handleCloseByEsc = e => {
-    if (e.code === 'Escape') {
-      this.props.closeModal();
-    }
-  };
+  useEffect(() => {
+    const handleCloseByEsc = e => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    };
+    window.addEventListener('keydown', handleCloseByEsc);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleCloseByEsc);
-  }
+    return () => {
+      window.removeEventListener('keydown', handleCloseByEsc);
+    };
+  }, [closeModal]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleCloseByEsc);
-  }
-
-  render() {
-    const { item } = this.props;
-    return createPortal(
-      <Backdrop onClick={this.handleBackdropClose}>
-        <ModalContainer>
-          <img src={item.largeImageURL} alt={item.tags} />
-        </ModalContainer>
-      </Backdrop>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <Backdrop onClick={handleBackdropClose}>
+      <ModalContainer>
+        <img src={item.largeImageURL} alt={item.tags} />
+      </ModalContainer>
+    </Backdrop>,
+    modalRoot
+  );
+};
